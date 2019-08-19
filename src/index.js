7246577b3ebd046fbb35bddb3cf16f5e24730566
yottanami <yottanami@gmail.com>
2019-08-19 16:21:57 +0430

    Init commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import UploadPost from './screens/UploadPost';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
