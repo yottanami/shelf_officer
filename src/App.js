c78a2ac8f1762ff9c31dc165aa9fb0b2f5117eae
yottanami <yottanami@gmail.com>
2019-10-22 17:38:18 +0330

    Create login using fakeAuth

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
 import React from 'react';
 import apolloClient from './apolloSetup';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './components/nav';
-
 import Categories from './pages/categories/Categories';
 import CreateCategory from './pages/categories/CreateCategory';
 import EditCateogry from './pages/categories/EditCategory';
-
 import Posts from './pages/posts/Posts';
 import CreatePost from './pages/posts/CreatePost';
 import EditPost from './pages/posts/EditPost';
+import {fakeAuth, Login} from './pages/auth/Login';
+
+import {
+  BrowserRouter,
+  Router,
+  Link,
+  Switch,
+  Route,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
+
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props)=>(
+    fakeAuth.isAuthenticated === true
+    ? <Component {...props}/>
+    : <Redirect to={{
+      pathname: '/login',
+      state: { from: props.location }
+    }}/>
+  )}/>
+)
+
+const AuthButton = withRouter(({ history }) => (
+  fakeAuth.isAuthenticated === true
+  ? <p>
+    Welcome! <button onClick={()=> {
+      fakeAuth.signout(()=> history.push('/'))
+    }}>Sign Out</button>
+  </p>
+  : <p>You are not logged in.</p>
+))
 
 const App = () => (
   <ApolloProvider client={apolloClient}>
-  <Router>
-    <div>
-      <Nav/>
-      <Route exact path='/' component={Categories} />
-      <Route path='/categories' component={Categories} />
-      <Route path='/categories/:id/edit' component={EditCateogry} />
-      <Route path='/categories/add' component={CreateCategory} />
+    <BrowserRouter>
+      <div>
+        <Nav/>
+        <AuthButton />
+        <PrivateRoute exact path='/' component={Categories} />
+        <PrivateRoute path='/categories' component={Categories} />
+        <PrivateRoute path='/categories/:id/edit' component={EditCateogry} />
+        <PrivateRoute path='/categories/add' component={CreateCategory} />
 
-      <Route path='/posts/:categoryId' component={Posts} />
-      <Route path='/posts/:categoryId/edit/:id' component={EditPost} />
-      <Route path='/posts/:categoryId/add' component={CreatePost} />
+        <PrivateRoute path='/posts/:categoryId' component={Posts} />
+        <PrivateRoute path='/posts/:categoryId/edit/:id' component={EditPost} />
+        <PrivateRoute path='/posts/:categoryId/add' component={CreatePost} />
 
-    </div>
-  </Router>
+        <Route path='/login' component={Login} />
+
+      </div>
+    </BrowserRouter>
   </ApolloProvider>
 );
 
